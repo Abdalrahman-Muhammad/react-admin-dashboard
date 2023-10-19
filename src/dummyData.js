94670e772083e5dab934ef49159aaa5d0c7e4dfe
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-10-19 23:27:23 +0300

    Add: ummy data for the chart

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    month: "Jan",
+    "Active User": 4000,
+  },
+  {
+    month: "Feb",
+    "Active User": 3000,
+  },
+  {
+    month: "Mar",
+    "Active User": 2000,
+  },
+  {
+    month: "Apr",
+    "Active User": 1000,
+  },
+  {
+    month: "May",
+    "Active User": 500,
+  },
+  {
+    month: "Jun",
+    "Active User": 400,
+  },
+  {
+    month: "Jul",
+    "Active User": 3000,
+  },
+  {
+    month: "Aug",
+    "Active User": 200,
+  },
+  {
+    month: "Sep",
+    "Active User": 100,
+  },
+  {
+    month: "Oct",
+    "Active User": 550,
+  },
+  {
+    month: "Nov",
+    "Active User": 470,
+  },
+  {
+    month: "Dec",
+    "Active User": 3000,
+  },
+];
